f4df92fd389f20a186f2f9b30730bb865a8bc440
Bernardo Rodrigues da Silva <100239024+CalangoMolhado@users.noreply.github.com>
2023-12-28 03:27:40 -0300

    kind of hard of using strings in this language

---
+++ solves/camel_case.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string to_camel_case(string text)
+{
+    string msg;
+    bool capitalizeNext = false;
+
+    for (char c : text)
+    {
+        if (c == '-' || c == '_')
+        {
+            capitalizeNext = true;
+        }
+        else
+        {
+            if (capitalizeNext)
+            {
+                msg += toupper(c);
+                capitalizeNext = false;
+            }
+            else
+            {
+                msg += c;
+            }
+        }
+    }
+
+    return msg;
+}
