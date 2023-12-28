b4b5119aedbd5c7e16a67949a017cc138bb31dee
Bernardo Rodrigues da Silva <100239024+CalangoMolhado@users.noreply.github.com>
2023-12-28 01:48:46 -0300

    home alone

---
+++ solves/sum_of_digital_roots.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int digital_root(int n)
+{
+    int n_digits=0;
+    int sum=0;
+    int aux=n;
+
+    while(n>=10)
+    {
+        aux=n;
+        sum=0;
+        n_digits=0;
+
+        do
+        {
+            aux/=10;
+            n_digits++;
+        }while(aux!=0);
+        for(int a=0; a<n_digits;a++)
+        {    
+            sum+=n%10;
+            aux=(n-n%10)/10;
+            n=aux;    
+        }    
+        n=sum;
+    }
+    printf("%d",sum);
+    return n;
+}
