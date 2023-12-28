6f97667b19aa7ec155f3881e372619260623af2f
Bernardo Rodrigues da Silva <bernardo.rodsilva@gmail.com>
2023-12-28 16:14:14 -0300

    esse tempo é chato

---
+++ solves/middle_permutation.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+import math
+
+def middle_permutation(string):
+    length = len(string)
+    total_permutations = math.factorial(length)
+
+    if total_permutations % 2 == 0:
+        index = total_permutations // 2 - 1
+    else:
+        index = total_permutations // 2
+
+    result = ''
+    chars = sorted(list(string))
+    for i in range(length):
+        total_permutations //= length - i
+        current_index = index // total_permutations
+        result += chars.pop(current_index)
+        index %= total_permutations
+
+    return result
